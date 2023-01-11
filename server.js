b66c1e800a66bd31df87b89bb24917d95c61fce9
井方 <jing_fang@topsec.com.cn>
2023-01-11 18:05:16 +0800

    feat: 借助ztree实现了文章目录功能；添加了.gitignore；引入了open，启动服务可直接打开浏览器；优化了部分样式

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const {app, Logger} = require('jj.js');
+const open = require('open');
 
 //server
+open('http://localhost:3003');
 app.run(3003, '0.0.0.0', function(err){
-    !err && Logger.info('http server is ready on 3003');
+    !err && console.info('http server is ready on 3003');
 });
